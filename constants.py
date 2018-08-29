d1f0ba4269dc492324ec8c774904b5aaf6d04a11
akd13 <akankshitadash@yahoo.com>
2018-08-30 02:47:15 +0800

    Plotting initial

--- constants.py
+++ constants.py
@@ -2,3 +2,10 @@ TXREADS = 'txReads'
 TXRPKM = 'txRPKM'
 CDREADS = 'cdReads'
 CDRPKM = 'cdRPKM'
+
+TXREADS_LIST = ['txReads0','txReads1','txReads2']
+TXRPKM_LIST = ['txRPKM0','txRPKM1','txRPKM2']
+CDREADS_LIST = ['cdReads0','cdReads1','cdReads2']
+CDRPKM_LIST = ['cdRPKM0','cdRPKM1','cdRPKM2']
+
+
--- data.py
+++ data.py
@@ -2,28 +2,16 @@ import pandas as pd
 from constants import *
 import matplotlib.pyplot as plt
 
-df_main = pd.read_csv('Data/T0.txt', sep=",")
-df_2 = pd.read_csv('Data/T1.txt', sep=",")
-df_3 = pd.read_csv('Data/T2.txt', sep=",")
+def read_values(index,list):
+	list_values = []
+	for l in list:
+		list_values.append(df_main.iloc[index][l])
+	return list_values
 
-# Merge dataframes
+df_main = pd.read_csv('Data/merged.txt', sep=",")
 
-#Merge T1
-df_main[TXREADS + '1'] = df_2[TXREADS + '1']
-df_main[TXRPKM + '1'] = df_2[TXRPKM + '1']
-df_main[CDREADS + '1'] = df_2[CDREADS + '1']
-df_main[CDRPKM + '1'] = df_2[CDRPKM + '1']
+for i in range(100):
+	plt.plot(TXREADS_LIST, read_values(i,TXREADS_LIST), label = df_main.iloc[i]['GeneName'])
 
-#Merge T2
-df_main[TXREADS + '2'] = df_3[TXREADS + '2']
-df_main[TXRPKM + '2'] = df_3[TXRPKM + '2']
-df_main[CDREADS + '2'] = df_3[CDREADS + '2']
-df_main[CDRPKM + '2'] = df_3[CDRPKM + '2']
-
-df_main.to_csv(path_or_buf='merged.txt', index=False)
-print(list(df_main))
-print(df_main.iloc[1]['txReads0'])
-
-print(len(df_main))
-# plt.plot(['txReads0', 'txReads1', 'txReads2'],[1,2,3])
-# plt.show()
+plt.legend(loc = 'upper right')
+plt.show()
