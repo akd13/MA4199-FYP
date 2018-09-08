c485903522ab467333a918ba3de8f3b56a29bc4b
akd13 <akankshitadash@yahoo.com>
2018-09-08 18:44:44 +0800

    Preprocessing all files

--- data.py
+++ data.py
@@ -16,11 +16,14 @@ df_main = pd.read_csv('Data/merged.txt', sep=",")
 for i, var_plot in enumerate(PLOT):
 	var_plot_list = PLOT_LIST[i]
 	fig = plt.subplot(111)
-	for i in range(20): # Plot first 20 values
-		fig.plot(var_plot_list, read_values(i, var_plot_list), label=df_main.iloc[i]['GeneName'])
+	for j in range(20): # Plot first 20 values
+		fig.plot(var_plot_list, read_values(j, var_plot_list), label=df_main.iloc[j]['GeneName'])
+		if(j%1000==0):
+			print(j)
+
 	chartBox = fig.get_position()
 	fig.set_position([chartBox.x0, chartBox.y0, chartBox.width * 0.6, chartBox.height])
 	fig.legend(loc='upper center', bbox_to_anchor=(1.45, 1.1), shadow=True, ncol=1)
 	plt.title(var_plot)
-	plt.savefig('Plots/' + var_plot)
+	# plt.savefig('Plots/' + var_plot+'all')
 	plt.show()
