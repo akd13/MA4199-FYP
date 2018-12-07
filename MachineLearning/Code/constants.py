69038e565b7effd348fc88b1e09e130faa8ba209
akd13 <akankshitadash@yahoo.com>
2018-12-07 23:14:17 +0800

    Rename

---
+++ MachineLearning/Code/constants.py
@@ -0,0 +1,12 @@
+TXREADS = 'txReads'
+TXRPKM = 'txRPKM'
+CDREADS = 'cdReads'
+CDRPKM = 'cdRPKM'
+TIMEPOINTS = [0,1,2]
+TXREADS_LIST = ['txReads0','txReads1','txReads2']
+TXRPKM_LIST = ['txRPKM0','txRPKM1','txRPKM2']
+CDREADS_LIST = ['cdReads0','cdReads1','cdReads2']
+CDRPKM_LIST = ['cdRPKM0','cdRPKM1','cdRPKM2']
+
+PLOT = [CDRPKM]
+PLOT_LIST = [CDRPKM_LIST]
